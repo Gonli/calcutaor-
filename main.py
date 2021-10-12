b8ed65687e70a232dfb3081c28f292754acc1b50
Gonli <92286572+Gonli@users.noreply.github.com>
2021-10-12 05:02:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Taking numbers from the first user
+
+a = input("enter your first number:-")
+
+# Taking operator from the user
+op = input("enter your opeartion:-")
+
+# Taking numbers from the second user
+b = input("enter your second number:-")
+
+# processing the operation on data
+answer = eval(a+op+b)
+
+# printing the answer on the processing data
+print("your answer is :-" + answer)
